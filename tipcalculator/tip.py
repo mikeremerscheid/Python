4c021cd7ecc884db204bf48505c0896c1a224e26
mikeremerscheid <mikeremerscheid@gmail.com>
2020-09-29 15:20:20 -0400

    second commit

--- tipcalculator/tip.py
+++ tipcalculator/tip.py
@@ -1,3 +1,12 @@
 from tkinter import Tk, Radiobutton, Button, Label, StringVar, IntVar, Entry
 
+class TipCalculator():
+    def __init__(self):
+        window = Tk()
+        window.title("Tip Calculator App")
+        window.configure(background="sky blue")
+        window.geometry("375x250")
+        window.resizable(width=False,height=False)
+        window.mainloop()
 
+TipCalculator()
