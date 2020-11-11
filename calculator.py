9aad6d53166d9956e58d4995926ea7d0ef310612
mikeremerscheid <mikeremerscheid@gmail.com>
2020-11-10 22:34:01 -0500

    Added a few programs

---
+++ calculator.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+class Application(Frame):
+    
+    def _init_(self,master)
+    
+    super(Application,self)
+    
+        super(Application,self)._init_(master)
+        self.task=""
+        self.UserIn = StringVar()
+        self.grid()
+        self.create_widgets()
